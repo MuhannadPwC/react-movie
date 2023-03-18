63825a56aefd4bfa1144f80378c3b12f886649ea
Muhannad Al Omari <muhannad.al-omari@pwc.com>
2023-03-18 22:30:54 +0300

    add fav and watch later no redux

--- Frontend/src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import watchlistReducer from "../features/WatchSlice"
-export const store = configureStore({
-  reducer: {
-    watchlistReducer 
-  },
-});
